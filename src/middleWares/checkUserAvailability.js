b4bdfbfac38ea2f93ecbde2309fdc8b790ad8929
mustafaahmed-f <mostafafikry97@gmail.com>
2023-10-03 15:04:09 +0300

    3oct commit

--- src/middleWares/checkUserAvailability.js
+++ src/middleWares/checkUserAvailability.js
@@ -1,17 +1,17 @@
 import userModel from "../../DB/models/userModel.js";
 import { asyncHandler } from "../utils/errorHandling.js";
 
+export const checkAvailability = asyncHandler(async (req, res, next) => {
+  const user = await userModel.findById(req.user.id);
 
+  if (user.isDeleted == true) {
+    return next(new Error("Account is deleted . Login to recover it !"), {
+      cause: 400,
+    });
+  }
+  if (user.available == false) {
+    return next(new Error("You must login first !!"), { cause: 400 });
+  }
 
-export const checkAvailability = asyncHandler(
-    async (req,res,next)=>{
-      
-        const user = await userModel.findById(req.user.id)
-            
-        if(user.available==false){return next(new Error("You must login first !!") , {cause:400})} 
-        if(user.isDeleted==true){return next(new Error("Account is deleted . Login to recover it !") , {cause:400})} 
-        
-        return next()
-    }
-)
-
+  return next();
+});
